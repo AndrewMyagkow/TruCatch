89bafb74bf127ff56a7c93a1ae6b820853cff01b
AndrewMyagkow <andrey.myagkov.90@mail.ru>
2022-12-06 16:52:46 +0300

    added an entry from a file

--- src/Main.java
+++ src/Main.java
@@ -1,57 +1,65 @@
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+
+
 public class Main {
-    public static void main(String[] args)
-    {
-       try {
-           Scanner sc = new Scanner(System.in);
-           String str = sc.nextLine();
-           String[] words = str.split(" ");
+    public static void main(String[] args) {
 
-           int y;
-           int x;
-           try {
-               x = Integer.parseInt(words[2]);
-               y = Integer.parseInt(words[0]);
-           } catch (Exception ex) {
-               throw new Exception("Error! Not number");
-           }
+            try {
+                File file = new File("D:\\hel.txt");
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                String line;
+                String str = null;
+                while ((line = br.readLine()) != null) {
+                    str = line;
+                }
+                int y;
+                int x;
+                assert str != null;
+                String[] words = str.split(" ");
+                try {
+                    x = Integer.parseInt(words[2]);
+                    y = Integer.parseInt(words[0]);
+                } catch (Exception ex) {
+                    throw new Exception("Error! Not number");
+                }
+                if ((!words[1].equals("+")) & (!words[1].equals("-")) & (!words[1].equals("*")) & (!words[1].equals("/"))) {
+                    throw new Exception("Operation Error!");
+                }
 
+                if ((x == 0) && (words[1].equals("/"))) {
+                    throw new Exception("Error! Division by zero");
 
-           if ((y == 0) && (words[1].equals("/"))) {
-               throw new Exception("Error! Division by zero");
-
-           }
-
-           if ((!words[1].equals("+"))&(!words[1].equals("-"))&(!words[1].equals("*"))&(!words[1].equals("/")))
-           {
-               throw  new Exception("Operation Error!" );
-           }
-            double y1 = y;
-           double x1 = x;
-           if(words[1].equals("+"))
-           {
-               System.out.println(y1+x1);
-           }
-           if(words[1].equals("-"))
-           {
-               System.out.println(y1-x1);
-           }
-           if(words[1].equals("*"))
-           {
-               System.out.println(y1*x1);
-           }
-           if(words[1].equals("/"))
-           {
-               System.out.println(y1/x1);
-
-           }
+                }
 
 
-       }
-       catch (Exception ex)
-       {
-           System.out.println(ex.getMessage());
-       }
+                double y1 = y;
+                double x1 = x;
+                if (words[1].equals("+")) {
+                    System.out.println(y1 + x1);
+                }
+                if (words[1].equals("-")) {
+                    System.out.println(y1 - x1);
+                }
+                if (words[1].equals("*")) {
+                    System.out.println(y1 * x1);
+                }
+                if (words[1].equals("/")) {
+                    System.out.println(y1 / x1);
 
-       }
-}
+                }
+
+
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+
+        }
+    }
+
+
+
+
